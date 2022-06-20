a5ba907f5efbbc20a72725df00c9853b4a6ae7b5
sunman54 <m.sunman@yandex.com>
2022-06-20 17:21:35 +0300

    next commit

--- uniTercih/urls.py
+++ uniTercih/urls.py
@@ -23,7 +23,7 @@ from home.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', home_view),
+    path('', home_view, name='home'),
 
     path('university', include('university.urls')),
 
